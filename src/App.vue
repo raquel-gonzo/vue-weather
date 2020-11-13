824970e0a520df5ffc2c976d3fae10f8787393b2
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-11-13 00:15:29 -0600

    template, have an environment variable for open weather api. first commit.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search ..." />
+      </div>
+
+      <div id="location">Chicago, IL</div>
+      <div id="date">Friday, November 13, 2020</div>
+      <div id="temp">40 degrees F</div>
+      <div id="weather">Light Rain</div>
+
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      api_key: process.env.VUE_APP_API_KEY
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: 0;
+}
+
+.search-box,
+.search-bar {
+  display: block;
+  width: 90%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  background-color: rgba(255, 255, 255, 0.5);
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+body {
+  font-family: "montserrat", sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
 }
 </style>
