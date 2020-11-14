c379018eaf9ae36aa1a48cf8d8b4f9014fcdc74f
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-11-13 22:19:16 -0600

    added search button, fetchWeather method

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,19 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search ..." />
+        <input
+          v-model="query"
+          type="text"
+          class="search-bar"
+          placeholder="Search ..."
+        />
+        <button @click.prevent="fetchWeather" class="btn-search">search</button>
       </div>
 
       <div id="location">Chicago, IL</div>
       <div id="date">Friday, November 13, 2020</div>
       <div id="temp">40 degrees F</div>
       <div id="weather">Light Rain</div>
-
     </main>
   </div>
 </template>
@@ -19,8 +24,21 @@ export default {
   name: "App",
   data() {
     return {
-      api_key: process.env.VUE_APP_API_KEY
+      api_key: process.env.VUE_APP_API_KEY,
+      url_base: "https://api.openweathermap.org/data/2.5",
+      query: "",
+      weather: {},
     };
+  },
+  methods: {
+    fetchWeather() {
+      fetch(`${this.url_base}/weather?q=${this.query}&appid=${this.api_key}`)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          this.weather = data;
+        });
+    },
   },
 };
 </script>
@@ -34,12 +52,17 @@ export default {
 
 .search-box,
 .search-bar {
-  display: block;
+  display: inline;
   width: 90%;
   padding: 15px;
   color: #313131;
   font-size: 20px;
   background-color: rgba(255, 255, 255, 0.5);
+}
+
+.btn-search {
+  display: inline;
+  font-size: 20px;
 }
 
 .search-box {
