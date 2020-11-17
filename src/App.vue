b310113b8345a9b57735789f2c88a86ecfaebb71
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-11-16 23:12:58 -0600

    more styles added.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <!-- <div id="container"> -->
       <div class="search-box">
         <input
           v-model="query"
@@ -11,13 +10,12 @@
         <button :disabled="searchButtonDisabled" @click.prevent="fetchWeather" class="rainbow-button">search</button>
       </div>
 
-      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+      <div class="weather-wrap fade-in" v-if="typeof weather.main != 'undefined'">
         <div id="location">{{ weather.name }}, {{ weather.sys.country }}</div>
         <div id="temp">{{ weatherTemp }} &#176;F</div>
         <div id="description">{{ weather.weather[0].description }}</div>
         <img :src="weatherIcon" :alt="`weather icon for ${weather.weather[0].description}`"/>
       </div>
-    <!-- </div> -->
   </div>
 </template>
 
@@ -66,22 +64,82 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Fira+Code&display=swap');
 #app {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-content: center;
+  font-family: 'Fira Code', monospace;
 }
 
 body {
-  background-image: radial-gradient(circle, #6666b6, #736fbe, #7f78c6, #8c81ce, #988bd6, #9488d1, #9184cc, #8d81c7, #7a71b3, #6761a0, #54518d, #42427b);
+  background-color: black;
   height: 100vh;
 }
 
+input{
+  width: 60%;
+  font-size: medium;
+  font-family: 'Fira Code', monospace;
+  padding-left: 16px;
+}
+
+
 .search-box{
   padding: 8%;
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  height: 50px;
+}
+
+.weather-wrap{
+  background-color: lavender;
+  width: 80%;
+  margin: 0px auto;
+  text-align: center;
+  border: 4px solid transparent;
+  border-image: linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%);
+  border-image-slice: 1;
+  padding: 2%;
+}
+
+.fade-in {
+animation: fadeIn ease 0.5s;
+-webkit-animation: fadeIn ease 0.5s;
+-moz-animation: fadeIn ease 0.5s;
+-o-animation: fadeIn ease 0.5s;
+-ms-animation: fadeIn ease 0.5s;
+}
+@keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+
+@-moz-keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+
+@-webkit-keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+
+@-o-keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+
+@-ms-keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
 }
 
 button {
+  margin-left: 15px;
   border-radius: 5px;
   border-style: none;
   display:flex;
@@ -94,6 +152,7 @@ button {
   cursor: pointer;
   transition: border 0.3s;
   padding: 2px;
+  font-family: 'Fira Code', monospace;
 }
 
 button:hover:enabled {
