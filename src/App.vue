bd17558885ffaf8a93f5d37c7eebda4bbd758426
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-11-17 23:11:42 -0600

    added error handling

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
 <template>
   <div id="app">
-      <div class="search-box">
-        <input
-          v-model="query"
-          type="text"
-          class="search-bar"
-          placeholder="Search ..."
-        />
-        <button :disabled="searchButtonDisabled" @click.prevent="fetchWeather" class="rainbow-button">search</button>
-      </div>
+    <div class="search-box">
+      <input
+        v-model="query"
+        type="text"
+        class="search-bar"
+        placeholder="Search ..."
+      />
+      <button
+        :disabled="searchButtonDisabled"
+        @click.prevent="fetchWeather"
+        class="rainbow-button"
+      >
+        search
+      </button>
+    </div>
 
-      <div class="weather-wrap fade-in" v-if="typeof weather.main != 'undefined'">
-        <div id="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-        <div id="temp">{{ weatherTemp }} &#176;F</div>
-        <div id="description">{{ weather.weather[0].description }}</div>
-        <img :src="weatherIcon" :alt="`weather icon for ${weather.weather[0].description}`"/>
-      </div>
+    <div class="weather-wrap fade-in" v-if="typeof weather.main != 'undefined'">
+      <div id="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+      <div id="temp">{{ weatherTemp }} &#176;F</div>
+      <div id="description">{{ weather.weather[0].description }}</div>
+      <img
+        :src="weatherIcon"
+        :alt="`weather icon for ${weather.weather[0].description}`"
+      />
+    </div>
+
+    <div v-if="errorFound" class="weather-wrap">check spelling</div>
+
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "App",
   data() {
@@ -28,49 +42,54 @@ export default {
       url_base: "https://api.openweathermap.org/data/2.5",
       query: "",
       weather: {},
+      errorFound: false
     };
   },
   methods: {
     fetchWeather() {
-      fetch(
-        `${this.url_base}/weather?q=${this.query}&units=imperial&appid=${this.api_key}`
-      )
-        .then((res) => {
-          return res.json();
+      axios.get(`${this.url_base}/weather?q=${this.query}&units=imperial&appid=${this.api_key}`)
+        .then(res => {
+          this.errorFound = false;
+          this.setResults(res.data);
+          return res.data;
         })
-        .then(this.setResults);
+        .catch(error => {
+          this.errorFound  = true;
+          console.error(error);
+        })
+
     },
 
-    setResults(results) {
-      this.weather = results;
-      console.log(this.weather);
+    setResults(result) {
+      this.weather = result;
     },
   },
   computed: {
     weatherTemp() {
+      console.log(this.weather);
       if (this.weather.main.temp) {
         return this.weather.main.temp.toFixed();
       }
       return "";
     },
     weatherIcon() {
-     return `http://openweathermap.org/img/wn/${this.weather.weather[0].icon}@2x.png`
+      return `http://openweathermap.org/img/wn/${this.weather.weather[0].icon}@2x.png`;
     },
     searchButtonDisabled() {
       return this.query === "";
-    }
+    },
   },
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Fira+Code&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Fira+Code&display=swap");
 #app {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-content: center;
-  font-family: 'Fira Code', monospace;
+  font-family: "Fira Code", monospace;
 }
 
 body {
@@ -78,15 +97,14 @@ body {
   height: 100vh;
 }
 
-input{
+input {
   width: 60%;
   font-size: medium;
-  font-family: 'Fira Code', monospace;
+  font-family: "Fira Code", monospace;
   padding-left: 16px;
 }
 
-
-.search-box{
+.search-box {
   padding: 8%;
   width: 80%;
   display: flex;
@@ -95,77 +113,114 @@ input{
   height: 50px;
 }
 
-.weather-wrap{
+.weather-wrap {
   background-color: lavender;
   width: 80%;
   margin: 0px auto;
   text-align: center;
   border: 4px solid transparent;
-  border-image: linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%);
+  border-image: linear-gradient(
+    to bottom right,
+    #b827fc 0%,
+    #2c90fc 25%,
+    #b8fd33 50%,
+    #fec837 75%,
+    #fd1892 100%
+  );
   border-image-slice: 1;
   padding: 2%;
 }
 
 .fade-in {
-animation: fadeIn ease 0.5s;
--webkit-animation: fadeIn ease 0.5s;
--moz-animation: fadeIn ease 0.5s;
--o-animation: fadeIn ease 0.5s;
--ms-animation: fadeIn ease 0.5s;
+  animation: fadeIn ease 0.5s;
+  -webkit-animation: fadeIn ease 0.5s;
+  -moz-animation: fadeIn ease 0.5s;
+  -o-animation: fadeIn ease 0.5s;
+  -ms-animation: fadeIn ease 0.5s;
 }
 @keyframes fadeIn {
-0% {opacity:0;}
-100% {opacity:1;}
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 
 @-moz-keyframes fadeIn {
-0% {opacity:0;}
-100% {opacity:1;}
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 
 @-webkit-keyframes fadeIn {
-0% {opacity:0;}
-100% {opacity:1;}
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 
 @-o-keyframes fadeIn {
-0% {opacity:0;}
-100% {opacity:1;}
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 
 @-ms-keyframes fadeIn {
-0% {opacity:0;}
-100% {opacity:1;}
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 
 button {
   margin-left: 15px;
   border-radius: 5px;
   border-style: none;
-  display:flex;
-  align-items:center;
-  justify-content:center;
-  text-transform:uppercase;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
   width: 125px;
   color: white;
   background-color: #302244;
   cursor: pointer;
   transition: border 0.3s;
   padding: 2px;
-  font-family: 'Fira Code', monospace;
+  font-family: "Fira Code", monospace;
 }
 
 button:hover:enabled {
   border: 4px solid transparent;
-  border-image: linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%);
+  border-image: linear-gradient(
+    to bottom right,
+    #b827fc 0%,
+    #2c90fc 25%,
+    #b8fd33 50%,
+    #fec837 75%,
+    #fd1892 100%
+  );
   border-image-slice: 1;
-  transition: border 0.3s;
+  transition: border 0.2s;
   padding: 2px;
+}
+
+button:focus {
+  outline: 0;
 }
 
 .rainbow-button:disabled {
   opacity: 0.5;
   cursor: not-allowed;
 }
-
 </style>
