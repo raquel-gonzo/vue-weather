9a98e3fed5716ee088a887f90794baf9377a055e
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-11-23 23:45:47 -0600

    unit toggle imperial / metric and designed toggle.

--- src/App.vue
+++ src/App.vue
@@ -33,13 +33,23 @@
     </div>
 
     <div class="weather-wrap fade-in" v-if="typeof weather.main != 'undefined'">
-      <div id="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-      <div id="temp">{{ weatherTemp }} &#176;F</div>
+      <div id="location">
+        <h2>{{ weather.name }}, {{ weather.sys.country }}</h2>
+      </div>
+      <div id="temp">
+        {{ weatherTemp }}
+        <span v-if="isMetric">&#176;C</span>
+        <span v-else>&#176;F</span>
+      </div>
       <div id="description">{{ weather.weather[0].description }}</div>
       <img
         :src="weatherIcon"
         :alt="`weather icon for ${weather.weather[0].description}`"
       />
+      <label class="switch">
+        <input v-model="isMetric" type="checkbox" />
+        <span class="slider round"></span>
+      </label>
     </div>
 
     <!-- v-model input examples -->
@@ -63,7 +73,7 @@ export default {
       query: "",
       weather: {},
       errorFound: false,
-      // inputData: "",
+      isMetric: false,
       selectData: "",
     };
   },
@@ -98,8 +108,11 @@ export default {
   computed: {
     weatherTemp() {
       console.log(this.weather);
-      if (this.weather.main.temp) {
-        return this.weather.main.temp.toFixed();
+      if (this.weather.main.temp && !this.isMetric) {
+        return this.weather.main.temp.toFixed(); // farenheit
+      } else if (this.weather.main.temp && this.isMetric) {
+        let celsius = ((this.weather.main.temp - 32) * (5 / 9)).toFixed(); // celsius
+        return celsius;
       }
       return "";
     },
@@ -261,5 +274,69 @@ button:focus {
 .rainbow-button:disabled {
   opacity: 0.5;
   cursor: not-allowed;
+}
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  /* background-color: #2196F3; */
+  background-color: #302244;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196f3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
 }
 </style>
