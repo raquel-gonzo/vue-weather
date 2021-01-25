ba5421173f215b0857ae30a168e95abbfb2371a2
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2021-01-24 18:32:32 -0600

    updated github project link

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="icons">
     <p>View this project on GitHub</p>
-    <a href="https://github.com/raquel-gonzo" target="_blank">
+    <a href="https://github.com/raquel-gonzo/vue-weather" target="_blank">
       <i class="fab fa-github-square fa-3x"></i>
     </a>
   </div>
